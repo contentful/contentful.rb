b1db52ab796677600782ce033e6896820dd71a76
Jan Lelis <mail@janlelis.de>
2014-03-02 14:29:26 +0100

    Contentful::Response

--- spec/response_spec.rb
+++ spec/response_spec.rb
@@ -1,5 +1,44 @@
 require 'spec_helper'
 
 describe Contentful::Response do
-  pending
+  let(:successful_response){ Contentful::Response.new raw_fixture('nyancat') }
+  let(:error_response){ Contentful::Response.new raw_fixture('not_found') }
+  let(:unparsable_response){ Contentful::Response.new raw_fixture('unparsable') }
+
+  describe "#raw" do
+    it "returns the raw response it has been initalized with" do
+      expect( successful_response.raw ).to eq raw_fixture('nyancat')
+    end
+  end
+
+  describe "#json" do
+    it "returns the repsonse's parsed json" do
+      expect( successful_response.json ).to eq json_fixture('nyancat')
+    end
+  end
+
+  describe "#status" do
+    it 'returns :ok for normal responses' do
+      expect( successful_response.status ).to eq :ok
+    end
+
+    it 'returns :contentful_error for error responses' do
+      expect( error_response.status ).to eq :contentful_error
+    end
+
+    it 'returns :unparsable_json for unparsable json responses' do
+      expect( unparsable_response.status ).to eq :unparsable_json
+    end
+  end
+
+  describe "#error_message" do
+    it 'returns contentful error message for contentful errors' do
+      expect( error_response.error_message ).to eq "The resource could not be found."
+    end
+
+    it 'returns json parser error message for json parse errors' do
+      expect( unparsable_response.error_message ).to include "unexpected token"
+    end
+  end
+
 end
