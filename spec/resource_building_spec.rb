a4954d7a9b0bc4fa9d358a6b727317b5906678bb
Jan Lelis <mail@janlelis.de>
2014-04-16 20:58:48 +0200

    improve link inclusion processing, prevent endless recursion, split up long method

--- spec/resource_building_spec.rb
+++ spec/resource_building_spec.rb
@@ -17,4 +17,13 @@ describe 'Resource Building Examples' do
     expect( resource.fields[:links] ).to be_a Array
     expect( resource.fields[:links].first ).to be_a Contentful::Entry
   end
+
+  it 'can also reference itself' do
+    response = Contentful::Response.new raw_fixture('self_link')
+    resource = Contentful::ResourceBuilder.new(create_client, response).run.first
+
+    other_resource = resource.fields[:e]
+    expect( other_resource ).to be_a Contentful::Entry
+    expect( other_resource.fields[:e] ).to be resource
+  end
 end
