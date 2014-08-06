f3c27798a8bbfc01181342c4a1a7644b6d05121e
Sascha Depold <sdepold@users.noreply.github.com>
2014-08-06 07:57:11 +0200

    fix wording

--- examples/resource_mapping.rb
+++ examples/resource_mapping.rb
@@ -1,7 +1,7 @@
 # Using the :resource_mapping configuration, you can register your own classes to be used
 # for the results from contentful.
 #
-# The key of the array defines the resource type (object["sys"]["type"]) and the value is:
+# The key of the resource_mapping hash defines the resource type (object["sys"]["type"]) and the value is:
 # - the Class to use
 # - a Proc, that returns the Class to use
 # - a Symbol for a method of the ResourceBuilder object
