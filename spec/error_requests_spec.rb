6a687094a137f7b80fd053de50b6d4067a2e89a3
David Litvak Bruno <david.litvakb@gmail.com>
2016-08-04 15:04:17 +0200

    Add Rate Limit Backoff

--- spec/error_requests_spec.rb
+++ spec/error_requests_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
+class NonCachingClient < Contentful::Client
+  def request_headers
+    headers = super
+    headers['Cf-No-Cache'] = 'foobar'
+    headers
+  end
+end
+
+class RetryLoggerMock < Logger
+  attr_reader :retry_attempts
+
+  def initialize(*)
+    super
+    @retry_attempts = 0
+  end
+
+  def info(message)
+    super
+    @retry_attempts += 1 if message.include?('Contentful API Rate Limit Hit! Retrying')
+  end
+end
+
 describe 'Error Requests' do
   it 'will return 404 (Unauthorized) if resource not found' do
     expect_vcr('not found')do
@@ -29,11 +51,30 @@ describe 'Error Requests' do
     skip
   end
 
-  it 'will return a 429 if the ratelimit is reached' do
-    client = Contentful::Client.new(space: 'wrong', access_token: 'credentials')
+  it 'will return a 429 if the ratelimit is reached and is not set to retry' do
+    client = Contentful::Client.new(space: 'wrong', access_token: 'credentials', max_rate_limit_retries: 0)
     expect_vcr('ratelimit') {
       client.entry('nyancat')
     }.to raise_error(Contentful::RateLimitExceeded)
+  end
+
+  it 'will retry on 429 by default' do
+    logger = RetryLoggerMock.new(STDOUT)
+    client = NonCachingClient.new(
+      api_url: 'cdnorigin.flinkly.com',
+      space: '164vhtp008kz',
+      access_token: '7699b6c6f6cee9b6abaa216c71fbcb3eee56cb6f082f57b5e21b2b50f86bdea0',
+      raise_errors: true,
+      logger: logger
+    )
+
+    vcr('ratelimit_retry') {
+      3.times {
+        client.assets
+      }
+    }
+
+    expect(logger.retry_attempts).to eq 1
   end
 
   it 'will return 503 (ServiceUnavailable) when the service is unavailable' do
